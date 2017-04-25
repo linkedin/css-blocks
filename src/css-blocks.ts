7f890ed8d0e37b6d7549f45a34461f9080c589e1
Chris Eppstein <chris@eppsteins.net>
2017-04-25 11:28:23 -0700

    Basic BEM handling of :block.

--- src/css-blocks.ts
+++ src/css-blocks.ts
@@ -1,4 +1,78 @@
-import greeting from './hello';
-export default function () {
-  return greeting;
+import * as postcss from "postcss";
+import * as path from "path";
+import * as selectorParser from "postcss-selector-parser";
+
+export namespace api {
+
+  export enum OutputMode {
+    BEM = 1
+  }
+
+  export interface PluginOptions {
+    readonly outputMode: OutputMode;
+  }
+
+  export class CssBlockError extends Error {
+  }
+
+  export class MissingSourcePath extends CssBlockError {
+    constructor() {
+      super("PostCSS `from` option is missing." +
+            " The source filename is required for CSS Blocks to work correctly.");
+    }
+  }
+}
+
+class OptionsReader implements api.PluginOptions {
+  private _outputMode: api.OutputMode;
+
+  constructor(opts: api.PluginOptions) {
+    this._outputMode = opts.outputMode || api.OutputMode.BEM;
+  }
+
+  get outputMode() {
+    return this._outputMode;
+  }
+  get outputModeName(): string {
+    return api.OutputMode[this.outputMode];
+  }
+}
+
+class Block {
+  private _name: string;
+
+  constructor(name: string) {
+    this._name = name;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+  set name(name: string) {
+    this._name = name;
+  }
+}
+
+export default function initializer(postcssImpl: typeof postcss) {
+  return postcssImpl.plugin("css-blocks", (pluginOptions: api.PluginOptions) => {
+    let opts = new OptionsReader(pluginOptions);
+    return (root, result) => {
+      let sourceFile;
+      if (result && result.opts && result.opts.from) {
+        sourceFile = result.opts.from;
+      } else {
+        throw new api.MissingSourcePath();
+      }
+      let block = new Block(path.parse(sourceFile).name);
+      root.walkRules((rule) => {
+        let selector =  selectorParser().process(rule.selector).res;
+        selector.walkPseudos((pseudo) => {
+          if (pseudo.value === ":block") {
+            pseudo.replaceWith(selectorParser.className({value: block.name}));
+          }
+        });
+        rule.selector = selector.toString();
+      });
+    }
+  });
 }
--- test/test.ts
+++ test/test.ts
@@ -1,11 +1,21 @@
 //import { suite, test, slow, timeout, skip, only } from "mocha-typescript";
 declare function require(name:string):any;
 import { suite, test } from "mocha-typescript";
-import cssBlocks from "../src/css-blocks";
+import { default as cssBlocksInitializer, api as cssBlocks } from "../src/css-blocks";
 import { assert } from "chai";
+
+import * as postcss from "postcss";
  
-@suite class Hello {
-  @test "world!"() {
-    assert.equal(cssBlocks(), "hello world!");
+@suite("In BEM output mode")
+class BEMOutputMode {
+  @test "replaces :block with the name of the block from filename"() {
+    let cssBlocksProcessor = cssBlocksInitializer(postcss)
+    let cssBlocksOpts: cssBlocks.PluginOptions = {
+      outputMode: cssBlocks.OutputMode.BEM,
+    };
+    let processOpts = { from: "foo/bar/test-1.scss" };
+    return postcss(cssBlocksProcessor(cssBlocksOpts)).process(":block { color: red; }", processOpts).then((result) => {
+      assert.equal(result.css.toString(), ".test-1 { color: red; }");
+    });
   }
 }
