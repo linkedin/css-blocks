134a5fe33d51024fdddcb3a63e7b7bc53899c806
Chris Eppstein <chris@eppsteins.net>
2017-04-25 12:35:02 -0700

    generalize the testing infrastructure. add a test for pseudoclasses on the :block.

--- test/test.ts
+++ test/test.ts
@@ -5,17 +5,43 @@ import { default as cssBlocksInitializer, api as cssBlocks } from "../src/css-bl
 import { assert } from "chai";
 
 import * as postcss from "postcss";
+import * as perfectionist from "perfectionist";
  
 @suite("In BEM output mode")
 class BEMOutputMode {
-  @test "replaces :block with the name of the block from filename"() {
+  process(filename: string, contents: string) {
+    let processOpts = { from: filename };
     let cssBlocksProcessor = cssBlocksInitializer(postcss)
     let cssBlocksOpts: cssBlocks.PluginOptions = {
       outputMode: cssBlocks.OutputMode.BEM,
     };
-    let processOpts = { from: "foo/bar/test-1.scss" };
-    return postcss(cssBlocksProcessor(cssBlocksOpts)).process(":block { color: red; }", processOpts).then((result) => {
-      assert.equal(result.css.toString(), ".test-1 { color: red; }");
+    return postcss([
+      cssBlocksProcessor(cssBlocksOpts),
+      perfectionist({format: "compact", indentSize: 2})
+    ]).process(contents, processOpts);
+  }
+
+  @test "replaces block withe the blockname from the file"() {
+    let filename = "foo/bar/test-1.scss";
+    let inputCSS = `:block {color: red;}`;
+    return this.process(filename, inputCSS).then((result) => {
+      assert.equal(
+        result.css.toString(),
+        ".test-1 { color: red; }\n"
+      );
+    });
+  }
+
+  @test "handles pseudoclasses on the :block"() {
+    let filename = "foo/bar/test-2.scss";
+    let inputCSS = `:block {color: #111;}
+                    :block:hover { font-weight: bold; }`;
+    return this.process(filename, inputCSS).then((result) => {
+      assert.equal(
+        result.css.toString(),
+        ".test-2 { color: #111; }\n" +
+        ".test-2:hover { font-weight: bold; }\n"
+      );
     });
   }
 }
