63f45b9375576ae49338f38f13ba315422d79c4a
Chris Eppstein <chris@eppsteins.net>
2017-05-16 13:53:09 -0700

    update examples.

--- examples/webpack/app/index.js
+++ examples/webpack/app/index.js
@@ -1,24 +1,26 @@
 import helloCSS from "./styles/hello.css";
 import californiaCSS from "./styles/california.css";
+import outlinesCSS from "./styles/outlines.css";
 
-import { iCssAdapter as cssBlockAdapter } from "css-blocks";
+import { iCssAdapter as cssBlockAdapter, union as stylesUnion } from "css-blocks";
 let helloStyles = cssBlockAdapter(helloCSS);
 let californiaStyles = cssBlockAdapter(californiaCSS);
+let outlinesStyles = cssBlockAdapter(outlinesCSS);
 
 
 
 function hello(name, styles) {
   var element = document.createElement('div');
-  element.className = styles(":block");
+  element.className = styles.block(".root");
   element.innerHTML = "Hello, ";
 
   var world = document.createElement('span');
   world.innerHTML = name;
-  world.className = styles(".greeting-target");
+  world.className = stylesUnion(styles.block(".greeting-target"), styles.outlines(".outlined"));
   element.appendChild(world);
 
   return element;
 }
 
-document.body.appendChild(hello("World", helloStyles));
-document.body.appendChild(hello("California", californiaStyles));
+document.body.appendChild(hello("World", {block: helloStyles, outlines: outlinesStyles}));
+document.body.appendChild(hello("California", {block: californiaStyles, outlines: outlinesStyles}));
