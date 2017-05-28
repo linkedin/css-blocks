55590b7d313fc51bd3a7749f56b197225cd6b635
Chris Eppstein <chris@eppsteins.net>
2017-05-28 15:45:39 -0700

    Expose more API for consuming apps and frameworks

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,32 @@
+export {
+  iCssAdapter
+} from "./runtime";
+export {
+  Block,
+  State,
+  BlockClass,
+  BlockObject,
+  PropertyConcerns
+} from "./Block";
+export {
+  default as BlockParser,
+  StateInfo
+} from "./BlockParser";
+export {
+  CssBlockError,
+  InvalidBlockSyntax,
+  MissingSourcePath
+} from "./errors";
+export {
+ SourceLocation
+} from "./SourceLocation";
+export {
+  PluginOptions,
+  OptionsReader as PluginOptionsReader,
+} from "./options";
+export {
+  OutputMode
+} from "./OutputMode";
+
 import cssBlocks = require("./cssBlocks");
-export { iCssAdapter } from "./runtime";
 export default cssBlocks;
