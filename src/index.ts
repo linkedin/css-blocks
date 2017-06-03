420ee6de4ef1708e1173c38abd0d4f11ede418bf
Chris Eppstein <chris@eppsteins.net>
2017-06-03 00:52:58 -0700

    Expose some missing public api and vendored deps

--- src/index.ts
+++ src/index.ts
@@ -37,6 +37,11 @@ export {
   ParsedSelector,
   CompoundSelector
 } from "./parseSelector";
+export {
+  ImportedFile,
+  Importer,
+  filesystemImporter
+} from "./importing";
 
 import cssBlocks = require("./cssBlocks");
 export default cssBlocks;
