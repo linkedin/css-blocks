a9d1a7da5a5ba4b0e7acb89d7b09110936e20616
Chris Eppstein <chris@eppsteins.net>
2017-06-10 15:13:02 -0700

    Move loader to it's own file and leave index alone.

--- packages/webpack-plugin/test/util/testPaths.ts
+++ packages/webpack-plugin/test/util/testPaths.ts
@@ -3,4 +3,4 @@ import * as path from "path";
 export const DIST_DIRECTORY = path.resolve(__dirname, "..", "..");
 export const FIXTURES_DIRECTORY = path.resolve(DIST_DIRECTORY, "..", "test", "fixtures");
 export const BLOCK_FIXTURES_DIRECTORY = path.resolve(FIXTURES_DIRECTORY, "blocks");
-export const BLOCK_LOADER_PATH = require.resolve("../../src/index.js");
+export const BLOCK_LOADER_PATH = require.resolve("../../src/loader.js");
