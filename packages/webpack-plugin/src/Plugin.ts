c93b80a0df2640227c645b4743674bda4e67f97f
Chris Eppstein <chris@eppsteins.net>
2017-06-11 14:52:23 -0700

    Bug fixes.

--- packages/webpack-plugin/src/Plugin.ts
+++ packages/webpack-plugin/src/Plugin.ts
@@ -1,4 +1,4 @@
-import Tapable from "tapable";
+import Tapable = require("tapable");
 import * as webpack from "webpack";
 import { TemplateAnalyzer, StyleAnalysis } from "css-blocks";
 
@@ -17,21 +17,28 @@ export class CssBlocksTemplateAnalysisPlugin extends Tapable implements webpack.
     compiler.plugin("watch-run", () => {
       this.analyzer.reset();
     });
-    compiler.plugin("before-compile", (params) => {
-      console.log(params);
+    // console.log("registering analysis plugin");
+    compiler.plugin("before-run", (_params, cb) => {
+      // console.log("starting analysis");
       this.analyzer.analyze().then((analysis: StyleAnalysis) => {
+        // console.log("analysis complete", analysis);
         this.applyPlugins("after-template-analysis", analysis);
+        cb();
       });
     });
   }
   afterTemplateAnalysis(handler: (analysis: StyleAnalysis) => any) {
     this.plugin("after-template-analysis", handler);
   }
+  onStartingTemplateAnalysis(handler: (analysis: Promise<StyleAnalysis>) => any) {
+    this.plugin("starting-template-analysis", handler);
+  }
 }
 
 /* exposes plugin events for optimizer to tap into */
 export class CssBlocksCompilerPlugin extends Tapable implements webpack.Plugin {
-  analysis: StyleAnalysis;
+  analysis: StyleAnalysis | undefined;
+  pendingAnalysis: Promise<StyleAnalysis> | undefined;
   apply(compiler: webpack.Compiler) {
     compiler.plugin("compilation", (_compilation) => {
     });
@@ -39,11 +46,25 @@ export class CssBlocksCompilerPlugin extends Tapable implements webpack.Plugin {
   setAnalysis(analysis: StyleAnalysis) {
     this.analysis = analysis;
   }
+  setPendingAnalysis(analysis: Promise<StyleAnalysis>) {
+    this.pendingAnalysis = analysis;
+  }
   getTemplateAnalysisHandler() {
     let self = this;
     return function(analysis: StyleAnalysis) {
-      console.log("compiler got analysis");
+      // console.log("compiler got analysis");
+      // console.log("Need to compile:");
+      // analysis.transitiveBlockDependencies().forEach(d => {
+      //   console.log(d.source);
+      // });
       self.setAnalysis(analysis);
+    };
+  }
+  getTemplateAnalysisStartedHandler() {
+    let self = this;
+    return function(analysis: Promise<StyleAnalysis>) {
+      // console.log("compiler got pending analysis");
+      self.setPendingAnalysis(analysis);
     };
   }
 }
@@ -51,8 +72,8 @@ export class CssBlocksCompilerPlugin extends Tapable implements webpack.Plugin {
 /* exposes plugin events for optimizer to tap into */
 export class CssBlocksOptimizerPlugin extends Tapable implements webpack.Plugin {
   apply(compiler: webpack.Compiler) {
-    compiler.plugin("compilation", (compilation) => {
-      console.log("compiler", compilation);
+    compiler.plugin("compilation", (_compilation) => {
+      // console.log("compiler", compilation);
     });
   }
 }
@@ -66,14 +87,19 @@ export class CssBlocksPlugin extends Tapable implements webpack.Plugin {
   }
 
   apply(compiler: webpack.Compiler) {
+    // console.log("setting up css block plugins");
+    // console.log(this.options);
     let analyzer: CssBlocksTemplateAnalysisPlugin | undefined;
     if (this.options.templateAnalyzer) {
       analyzer = new CssBlocksTemplateAnalysisPlugin(this.options.templateAnalyzer);
       let blockCompilerPlugin = new CssBlocksCompilerPlugin();
       analyzer.afterTemplateAnalysis(blockCompilerPlugin.getTemplateAnalysisHandler());
+      analyzer.onStartingTemplateAnalysis(blockCompilerPlugin.getTemplateAnalysisStartedHandler());
+      analyzer.apply(compiler);
+      blockCompilerPlugin.apply(compiler);
     }
-    compiler.plugin("compilation", (compilation) => {
-      console.log("coordinator", compilation);
+    compiler.plugin("compilation", (_compilation) => {
+      // console.log("coordinator", compilation);
     });
   }
 }
--- packages/webpack-plugin/src/index.ts
+++ packages/webpack-plugin/src/index.ts
@@ -1,2 +1,9 @@
 export { default as loader } from "./loader";
 export { LoaderOptions } from "./LoaderOptions";
+export {
+  CssBlocksCompilerPlugin,
+  CssBlocksOptimizerPlugin,
+  CssBlocksPlugin,
+  CssBlocksPluginOptions,
+  CssBlocksTemplateAnalysisPlugin
+} from "./Plugin";
