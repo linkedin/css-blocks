146eebfcad7dedfc30fe85a2f3d30c887f302506
Chris Eppstein <chris@eppsteins.net>
2017-08-10 15:27:29 -0700

    add grunt-release

---
+++ packages/webpack-plugin/Gruntfile.js
@@ -0,0 +1,15 @@
+const shell = require("shelljs");
+module.exports = function(grunt) {
+  grunt.initConfig({
+    release: {
+      options: {
+        npm: false,
+        afterRelease: ['publish']
+      }
+    },
+  });
+  grunt.loadNpmTasks('grunt-release');
+  grunt.registerTask('publish', "Publish to NPM", function() {
+    shell.exec('li-npm-publish');
+  });
+};
