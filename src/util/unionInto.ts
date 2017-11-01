1dec139949e6718020bd1745e9737d74920d6c04
Chris Eppstein <chris@eppsteins.net>
2017-10-31 22:16:33 -0700

    this utility is a bit more useful if it accepts an Iterable

--- src/util/unionInto.ts
+++ src/util/unionInto.ts
@@ -1,4 +1,4 @@
-export function unionInto<T>(target: Set<T>, ...sets: Set<T>[]) {
+export function unionInto<T>(target: Set<T>, ...sets: Iterable<T>[]) {
   for (let set of sets) {
     for (let v of set) {
       target.add(v);
