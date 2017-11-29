218b23e289bb462d8fd06a13c55712fd2c9052aa
Chris Eppstein <chris@eppsteins.net>
2017-11-29 15:58:00 -0800

    Move css-blocks to a package subdirectory.

--- rollup.config.js
+++
@@ -1,9 +0,0 @@
-import typescriptRollup from 'rollup-plugin-typescript';
-
-export default {
-  entry: './src/css-blocks.ts',
-
-  plugins: [
-    typescriptRollup({typescript: require('typescript')})
-  ]
-}
