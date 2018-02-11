a342c59bedc9681c386419f01e8a89735be9ce8c
Chris Eppstein <chris@eppsteins.net>
2018-02-11 10:56:04 -0800

    chore(lint): Better types for the regexpu type declaration.

--- packages/css-blocks/types-local/regexpu-core/index.d.ts
+++ packages/css-blocks/types-local/regexpu-core/index.d.ts
@@ -1,4 +1,13 @@
 export = regexpu_core;
 
-declare function regexpu_core(pattern: any, flags?: any, options?: any): any;
-
+interface RegExpUOpts {
+  unicodePropertyEscape: boolean;
+  dotAllFlag: boolean;
+  useUnicodeFlag: boolean;
+}
+/**
+* @param pattern the regular expression string
+* @param [flags] a string containing the letters i (ignore case), u (unicode), and s (dot matches newlines)
+* @returns a regular expression string that is suitable for passing to the JS RegExp constructor.
+**/
+declare function regexpu_core(pattern: string, flags?: string, options?: Partial<RegExpUOpts>): string;
