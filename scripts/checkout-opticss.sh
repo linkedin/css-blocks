d1b681045d1ec9b55d6c4c960dc1ac90f125aa78
Chris Eppstein <chris@eppsteins.net>
2018-05-05 19:36:20 -0700

    chore: Exit checkout script early if any steps fail.

--- scripts/checkout-opticss.sh
+++ scripts/checkout-opticss.sh
@@ -14,14 +14,14 @@ mkdir -p $(dirname $OPTICSS_DIR)
 if [ -d $OPTICSS_DIR ]
 then
   cd $OPTICSS_DIR
-  git fetch origin
-  git checkout $BRANCH
-  git pull
-  lerna bootstrap --registry=https://registry.npmjs.org/
+  git fetch origin || exit 1
+  git checkout $BRANCH || exit 1
+  git pull || exit 1
+  lerna bootstrap --ci --registry=https://registry.npmjs.org/ || exit 1
 else
-  git clone -b $BRANCH --depth 1 git@github.com:linkedin/opticss.git $OPTICSS_DIR
-  cd $OPTICSS_DIR
-  lerna bootstrap --ci --registry=https://registry.npmjs.org/
+  git clone -b $BRANCH --depth 1 https://github.com/linkedin/opticss.git $OPTICSS_DIR || exit 1
+  cd $OPTICSS_DIR || exit 1
+  lerna bootstrap --ci --registry=https://registry.npmjs.org/ || exit 1
 fi
-lerna run compile
+lerna run compile || exit 1
 cd -
