3dcd62065bc2a56ce2367f6a813379d06f16cbc2
Chris Eppstein <chris@eppsteins.net>
2019-09-29 13:49:56 -0700

    fix: Merge conflict was checked in.

--- packages/@css-blocks/vscode/src/test/index.ts
+++ packages/@css-blocks/vscode/src/test/index.ts
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
-
 import * as testRunner from 'vscode/lib/testrunner';
 
 testRunner.configure({
@@ -13,15 +7,3 @@ testRunner.configure({
 });
 
 module.exports = testRunner;
-||||||| merged common ancestors
-=======
-import * as testRunner from 'vscode/lib/testrunner';
-
-testRunner.configure({
-    ui: 'bdd',
-    useColors: true,
-    timeout: 100000
-});
-
-module.exports = testRunner;
->>>>>>> lang-server-spike
