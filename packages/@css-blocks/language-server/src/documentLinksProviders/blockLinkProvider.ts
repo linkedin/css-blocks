8a940d40c05cd04abb240b778b9f2c3f3791f261
Jackson Dean <jcdean@linkedin.com>
2019-10-31 16:34:53 -0700

    feat: Add document links provider.
    For css and sass, vscode provides a "jump to file" feature for @import and
    `url()` statements. For css-blocks, we only need to add this support for
    custom "at rules" (@block, @export). For `url()`, the builtin vscode css
    language server should still be providing that functionality.

---
+++ packages/@css-blocks/language-server/src/documentLinksProviders/blockLinkProvider.ts
@@ -0,0 +1,49 @@
+import * as path from "path";
+import { DocumentLinkParams, TextDocuments } from "vscode-languageserver";
+import { DocumentLink } from "vscode-languageserver-types";
+import { URI } from "vscode-uri";
+
+import { isBlockFile } from "../util/blockUtils";
+
+const LINK_REGEX = /from\s+(['"])([^'"]+)\1;/;
+
+export async function blockLinksProvider(documents: TextDocuments, params: DocumentLinkParams): Promise<DocumentLink[]> {
+  let { uri } = params.textDocument;
+
+  if (!isBlockFile(uri)) {
+    return [];
+  }
+
+  let document = documents.get(uri);
+
+  if (!document) {
+    return [];
+  }
+
+  let links: DocumentLink[] = [];
+  let lines = document.getText().split(/\r?\n/);
+  let blockDirPath = path.dirname(URI.parse(uri).fsPath);
+
+  lines.forEach((line, lineIndex) => {
+    let matches = line.match(LINK_REGEX);
+
+    if (!matches) {
+      return;
+    }
+
+    let relativeBlockReferencePath = matches[2];
+    let absoluteBlockReferencePath = path.resolve(blockDirPath, relativeBlockReferencePath);
+    let startColumn = line.indexOf(relativeBlockReferencePath);
+    let endColumn = startColumn + relativeBlockReferencePath.length;
+
+    links.push({
+      target: URI.file(absoluteBlockReferencePath).toString(),
+      range: {
+        start: { line: lineIndex, character: startColumn },
+        end: { line: lineIndex, character: endColumn },
+      },
+    });
+  });
+
+  return links;
+}
