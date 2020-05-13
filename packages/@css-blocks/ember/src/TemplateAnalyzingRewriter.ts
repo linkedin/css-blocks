bdb68a5249452213296735b7fd8aaeb1a8503986
Chris Eppstein <chris@eppsteins.net>
2020-05-13 12:40:30 -0700

    chore: Refactor the helper generation code for legibility.

--- packages/@css-blocks/ember/src/TemplateAnalyzingRewriter.ts
+++ packages/@css-blocks/ember/src/TemplateAnalyzingRewriter.ts
@@ -25,9 +25,6 @@ import * as debugGenerator from "debug";
 import { EmberAnalysis } from "./EmberAnalysis";
 import { HandlebarsTemplate, TEMPLATE_TYPE } from "./HandlebarsTemplate";
 
-const CLASSNAMES_HELPER = "-cssblocks-";
-const HELPER_VERSION = 0;
-
 const enum StyleCondition {
   STATIC = 1,
   BOOLEAN = 2,
@@ -62,6 +59,8 @@ export interface ASTPluginWithDeps extends ASTPlugin {
   dependencies?(relativePath: string): string[];
 }
 
+type ElementSourceAnalysis = ReturnType<TemplateElement["getSourceAnalysis"]>;
+
 export class TemplateAnalyzingRewriter implements ASTPluginWithDeps {
   visitor: NodeVisitor;
   visitors: NodeVisitor;
@@ -71,6 +70,7 @@ export class TemplateAnalyzingRewriter implements ASTPluginWithDeps {
   syntax: Syntax;
   elementAnalyzer: ElementAnalyzer<TEMPLATE_TYPE>;
   cssBlocksOpts: CSSBlocksConfiguration;
+  helperInvocation: HelperInvocationGenerator;
 
   constructor(template: HandlebarsTemplate, block: Block | undefined | null, analysis: EmberAnalysis, options: CSSBlocksConfiguration, syntax: Syntax) {
     this.elementCount = 0;
@@ -79,6 +79,7 @@ export class TemplateAnalyzingRewriter implements ASTPluginWithDeps {
     this.syntax = syntax;
     this.elementAnalyzer = new ElementAnalyzer(analysis, options);
     this.cssBlocksOpts = options;
+    this.helperInvocation = new HelperInvocationGenerator(syntax.builders);
     if (this.block) {
       this.visitor = {
         ElementNode: this.ElementNode.bind(this),
@@ -159,157 +160,25 @@ export class TemplateAnalyzingRewriter implements ASTPluginWithDeps {
     }
   }
 
-  buildStringValue(subExpression: false, value: string): AST.TextNode;
-  buildStringValue(subExpression: true, value: string): AST.StringLiteral;
-  buildStringValue(subExpression: boolean, value: string): AST.StringLiteral | AST.TextNode;
-  buildStringValue(subExpression: boolean, value: string): AST.StringLiteral | AST.TextNode {
-    if (subExpression) {
-      return this.syntax.builders.string(value);
-    } else {
-      return this.syntax.builders.text(value);
-    }
-  }
-
-  buildStringConcatExpr(strings: Array<string | AST.SubExpression | AST.StringLiteral>): AST.SubExpression {
-    return this.syntax.builders.sexpr(
-      this.syntax.builders.path("-css-blocks-concat"),
-      strings.map(s => {
-        if (typeof s === "string") {
-          return this.syntax.builders.string(s);
-        } else {
-          return s;
-        }
-      }),
-    );
-  }
-
-  buildStringConcatStmnt(strings: Array<string | AST.MustacheStatement | AST.TextNode>): AST.ConcatStatement {
-    return this.syntax.builders.concat(
-      strings.map(s => {
-        if (typeof s === "string") {
-          return this.syntax.builders.text(s);
-        } else {
-          return s;
-        }
-      }),
-    );
-  }
-
   buildClassValue(subExpression: false, analysis: TemplateElement, node?: AST.MustacheStatement): AST.MustacheStatement | AST.ConcatStatement | null;
   buildClassValue(subExpression: true, analysis: TemplateElement, node?: AST.SubExpression): AST.SubExpression | null;
   buildClassValue(subExpression: boolean, analysis: TemplateElement, node?: AST.SubExpression | AST.MustacheStatement): AST.SubExpression | AST.MustacheStatement | AST.ConcatStatement | null {
-    const { sexpr, path, mustache, number: num, string: str, null: nullNode } = this.syntax.builders;
-    let sourceAnalysis = analysis.getSourceAnalysis();
-    if (sourceAnalysis.size() === 0) {
-      return node || null;
-    }
+    const { sexpr, path, mustache } = this.syntax.builders;
     if (subExpression) {
       if (node && !isSubExpression(node)) {
         throw new Error(`Illegal node ${node.type}`);
       } else if (!node) {
-        node = sexpr(path(CLASSNAMES_HELPER));
-      } else {
-        node.path = path(CLASSNAMES_HELPER);
+        node = sexpr(path(HelperInvocationGenerator.CLASSNAMES_HELPER));
       }
+      return this.helperInvocation.build(analysis, node);
     } else {
       if (node && isSubExpression(node)) {
         throw new Error(`Illegal node ${node.type}`);
       } else if (!node) {
-        node = mustache(path(CLASSNAMES_HELPER));
-      } else {
-        node.path = path(CLASSNAMES_HELPER);
+        node = mustache(path(HelperInvocationGenerator.CLASSNAMES_HELPER));
       }
+      return this.helperInvocation.build(analysis, node);
     }
-    node.params.push(num(HELPER_VERSION));
-    let blocks = [...sourceAnalysis.blocksFound];
-    node.params.push(num(blocks.length));
-    for (let block of blocks) {
-      node.params.push(str(block.guid));
-      node.params.push(nullNode()); // this could be a block when we implement block passing
-    }
-    let styles = [...sourceAnalysis.stylesFound];
-    node.params.push(num(styles.length));
-    let styleIndices = new Map<Style, number>();
-    let i = 0;
-    for (let style of styles) {
-      styleIndices.set(style, i++);
-      node.params.push(num(indexOfBlock(blocks, style)));
-      node.params.push(num(style.index));
-    }
-    node.params.push(num(sourceAnalysis.size()));
-
-    for (let style of sourceAnalysis.staticStyles) {
-      node.params.push(num(StyleCondition.STATIC));
-      node.params.push(num(styleIndices.get(style)!));
-    }
-
-    for (let ternaryClass of sourceAnalysis.ternaryStyles) {
-      node.params.push(num(StyleCondition.TERNARY));
-      if (isMustacheStatement(ternaryClass.condition!)) {
-        node.params.push(mustacheToExpression(this.syntax.builders, ternaryClass.condition));
-      } else {
-        node.params.push(ternaryClass.condition!);
-      }
-      if (isTrueCondition(ternaryClass)) {
-        node.params.push(num(ternaryClass.whenTrue.length));
-        for (let cls of ternaryClass.whenTrue) {
-          node.params.push(num(styleIndices.get(cls)!));
-        }
-      } else {
-        // there are no classes applied if true
-        node.params.push(num(0));
-      }
-      if (isFalseCondition(ternaryClass)) {
-        node.params.push(num(ternaryClass.whenFalse.length));
-        for (let cls of ternaryClass.whenFalse) {
-          node.params.push(num(styleIndices.get(cls)!));
-        }
-      } else {
-        // there are no classes applied if false
-        node.params.push(num(0));
-      }
-    }
-
-    for (let dynamicAttr of sourceAnalysis.booleanStyles) {
-      node.params.push(num(StyleCondition.BOOLEAN));
-      if (isMustacheStatement(dynamicAttr.condition)) {
-        node.params.push(mustacheToExpression(this.syntax.builders, dynamicAttr.condition));
-      } else {
-        node.params.push(dynamicAttr.condition);
-      }
-      node.params.push(num(dynamicAttr.value.size));
-      for (let attr of dynamicAttr.value) {
-        node.params.push(num(styleIndices.get(attr)!));
-      }
-    }
-
-    for (let switchStyle of sourceAnalysis.switchStyles) {
-      let values = Object.keys(switchStyle.group);
-      node.params.push(num(StyleCondition.SWITCH));
-      if (switchStyle.disallowFalsy) {
-        node.params.push(num(FalsySwitchBehavior.error));
-      } else {
-        node.params.push(num(FalsySwitchBehavior.unset));
-      }
-      node.params.push(mustacheToStringExpression(this.syntax.builders, switchStyle.stringExpression!));
-      for (let value of values) {
-        let obj = switchStyle.group[value];
-        node.params.push(str(value));
-        // If there are values provided for this conditional, they are meant to be
-        // applied instead of the selected attribute group member.
-        if (switchStyle.value.size) {
-          node.params.push(num(switchStyle.value.size));
-          for (let val of switchStyle.value) {
-            node.params.push(num(styleIndices.get(val)!));
-          }
-        }
-        else {
-          node.params.push(num(1));
-          node.params.push(num(styleIndices.get(obj)!));
-        }
-      }
-    }
-    return node;
   }
 
   StyleOfHelper(node: AST.MustacheStatement | AST.SubExpression): void {
@@ -321,8 +190,6 @@ export class TemplateAnalyzingRewriter implements ASTPluginWithDeps {
       const names = attrNames.filter(name => name !== "class");
       throw cssBlockError(`Unexpected attribute(s) [${names.join(", ")}] in rewrite for style-of helper.`, node, this.template.relativePath);
     }
-    node.params = [];
-    node.hash.pairs = [];
     if (isMustacheStatement(node)) {
       this.buildClassValue(false, attrMap["class"], node);
     } else {
@@ -353,50 +220,206 @@ export class TemplateAnalyzingRewriter implements ASTPluginWithDeps {
   }
 }
 
-function mustacheToExpression(builders: Syntax["builders"], expr: AST.MustacheStatement): AST.Expression {
-  if (isPathExpression(expr.path)) {
-    if (expr.params.length === 0 && expr.hash.pairs.length === 0) {
+class HelperInvocationGenerator {
+  static CLASSNAMES_HELPER = "-cssblocks-";
+  static HELPER_VERSION = 0;
+
+  builders: Syntax["builders"];
+  constructor(builders: Syntax["builders"]) {
+    this.builders = builders;
+  }
+  build(analysis: TemplateElement, node: AST.MustacheStatement): AST.MustacheStatement | null;
+  build(analysis: TemplateElement, node: AST.SubExpression): AST.SubExpression | null;
+  build(analysis: TemplateElement, node: AST.SubExpression | AST.MustacheStatement): AST.SubExpression | AST.MustacheStatement | null {
+    const { path, number: num, hash } = this.builders;
+    let sourceAnalysis = analysis.getSourceAnalysis();
+    if (sourceAnalysis.size() === 0) {
+      return null;
+    }
+    node.path = path(HelperInvocationGenerator.CLASSNAMES_HELPER);
+    node.params = [];
+    node.hash = hash();
+    node.params.push(num(HelperInvocationGenerator.HELPER_VERSION));
+    let { blocks, blockParams } = this.buildBlockParams(sourceAnalysis);
+    node.params.push(...blockParams);
+    let { styleIndices, styleParams } = this.buildStyleParams(sourceAnalysis, blocks);
+    node.params.push(...styleParams);
+    node.params.push(...this.buildStaticParams(sourceAnalysis, styleIndices));
+    node.params.push(...this.buildTernaryParams(sourceAnalysis, styleIndices));
+    node.params.push(...this.buildBooleanParams(sourceAnalysis, styleIndices));
+    node.params.push(...this.buildSwitchParams(sourceAnalysis, styleIndices));
+    return node;
+  }
+
+  indexOfBlock(blocks: Array<Block>, style: Style) {
+    for (let i = 0; i < blocks.length; i++) {
+      if (style.block === blocks[i] || style.block.isAncestorOf(blocks[i])) {
+        return i;
+      }
+    }
+    throw new Error("[internal error] Block not found.");
+  }
+
+  buildBlockParams(sourceAnalysis: ElementSourceAnalysis): {blocks: Array<Block>; blockParams: Array<AST.Expression>} {
+    const { number: num, string: str, null: nullNode } = this.builders;
+    let blocks = [...sourceAnalysis.blocksFound];
+    let blockParams = new Array<AST.Expression>();
+    blockParams.push(num(blocks.length));
+    for (let block of blocks) {
+      blockParams.push(str(block.guid));
+      blockParams.push(nullNode()); // this could be a block when we implement block passing
+    }
+    return {blocks, blockParams};
+  }
+
+  buildStyleParams(sourceAnalysis: ElementSourceAnalysis, blocks: Array<Block>): {styleIndices: Map<Style, number>; styleParams: Array<AST.Expression>} {
+    const { number: num } = this.builders;
+    let styles = [...sourceAnalysis.stylesFound];
+    let styleParams = new Array<AST.Expression>();
+    styleParams.push(num(styles.length));
+    let styleIndices = new Map<Style, number>();
+    let i = 0;
+    for (let style of styles) {
+      styleIndices.set(style, i++);
+      styleParams.push(num(this.indexOfBlock(blocks, style)));
+      styleParams.push(num(style.index));
+    }
+    styleParams.push(num(sourceAnalysis.size()));
+    return {
+      styleIndices,
+      styleParams,
+    };
+  }
+
+  buildStaticParams(sourceAnalysis: ElementSourceAnalysis, styleIndices: Map<Style, number>): Array<AST.Expression> {
+    const { number: num } = this.builders;
+    let params = new Array<AST.Expression>();
+    for (let style of sourceAnalysis.staticStyles) {
+      params.push(num(StyleCondition.STATIC));
+      params.push(num(styleIndices.get(style)!));
+    }
+    return params;
+  }
+
+  buildTernaryParams(sourceAnalysis: ElementSourceAnalysis, styleIndices: Map<Style, number>): Array<AST.Expression> {
+    const { number: num } = this.builders;
+    let params = new Array<AST.Expression>();
+    for (let ternaryClass of sourceAnalysis.ternaryStyles) {
+      params.push(num(StyleCondition.TERNARY));
+      if (isMustacheStatement(ternaryClass.condition!)) {
+        params.push(this.mustacheToExpression(this.builders, ternaryClass.condition));
+      } else {
+        params.push(ternaryClass.condition!);
+      }
+      if (isTrueCondition(ternaryClass)) {
+        params.push(num(ternaryClass.whenTrue.length));
+        for (let cls of ternaryClass.whenTrue) {
+          params.push(num(styleIndices.get(cls)!));
+        }
+      } else {
+        // there are no classes applied if true
+        params.push(num(0));
+      }
+      if (isFalseCondition(ternaryClass)) {
+        params.push(num(ternaryClass.whenFalse.length));
+        for (let cls of ternaryClass.whenFalse) {
+          params.push(num(styleIndices.get(cls)!));
+        }
+      } else {
+        // there are no classes applied if false
+        params.push(num(0));
+      }
+    }
+    return params;
+  }
+
+  buildBooleanParams(sourceAnalysis: ElementSourceAnalysis, styleIndices: Map<Style, number>): Array<AST.Expression> {
+    const { number: num } = this.builders;
+    let params = new Array<AST.Expression>();
+    for (let dynamicAttr of sourceAnalysis.booleanStyles) {
+      params.push(num(StyleCondition.BOOLEAN));
+      if (isMustacheStatement(dynamicAttr.condition)) {
+        params.push(this.mustacheToExpression(this.builders, dynamicAttr.condition));
+      } else {
+        params.push(dynamicAttr.condition);
+      }
+      params.push(num(dynamicAttr.value.size));
+      for (let attr of dynamicAttr.value) {
+        params.push(num(styleIndices.get(attr)!));
+      }
+    }
+    return params;
+  }
+
+  buildSwitchParams(sourceAnalysis: ElementSourceAnalysis, styleIndices: Map<Style, number>): Array<AST.Expression> {
+    const { number: num, string: str } = this.builders;
+    let params = new Array<AST.Expression>();
+
+    for (let switchStyle of sourceAnalysis.switchStyles) {
+      let values = Object.keys(switchStyle.group);
+      params.push(num(StyleCondition.SWITCH));
+      if (switchStyle.disallowFalsy) {
+        params.push(num(FalsySwitchBehavior.error));
+      } else {
+        params.push(num(FalsySwitchBehavior.unset));
+      }
+      params.push(this.mustacheToStringExpression(this.builders, switchStyle.stringExpression!));
+      for (let value of values) {
+        let obj = switchStyle.group[value];
+        params.push(str(value));
+        // If there are values provided for this conditional, they are meant to be
+        // applied instead of the selected attribute group member.
+        if (switchStyle.value.size) {
+          params.push(num(switchStyle.value.size));
+          for (let val of switchStyle.value) {
+            params.push(num(styleIndices.get(val)!));
+          }
+        }
+        else {
+          params.push(num(1));
+          params.push(num(styleIndices.get(obj)!));
+        }
+      }
+    }
+    return params;
+  }
+
+  mustacheToExpression(builders: Syntax["builders"], expr: AST.MustacheStatement): AST.Expression {
+    if (isPathExpression(expr.path)) {
+      if (expr.params.length === 0 && expr.hash.pairs.length === 0) {
+        return expr.path;
+      } else {
+        return builders.sexpr(expr.path, expr.params, expr.hash);
+      }
+    } else if (isStringLiteral(expr.path)) {
       return expr.path;
     } else {
-      return builders.sexpr(expr.path, expr.params, expr.hash);
-    }
-  } else if (isStringLiteral(expr.path)) {
-    return expr.path;
-  } else {
-    return expr.path;
-  }
-}
-
-function mustacheToStringExpression(builders: Syntax["builders"], stringExpression: Exclude<StringAST, null>): AST.Expression {
-  if (isConcatStatement(stringExpression)) {
-    return builders.sexpr(
-      builders.path(CONCAT_HELPER_NAME),
-      stringExpression.parts.reduce(
-        (arr, val) => {
-          if (val.type === "TextNode") {
-            arr.push(builders.string(val.chars));
-          } else {
-            arr.push(val.path);
-          }
-          return arr;
-        },
-        new Array<AST.Expression>()));
-  } else if (isSubExpression(stringExpression)) {
-    return stringExpression;
-  } else if (isPathExpression(stringExpression)) {
-    return builders.sexpr(stringExpression);
-  } else if (isMustacheStatement(stringExpression)) {
-    return mustacheToExpression(builders, stringExpression);
-  } else {
-    return assertNever(stringExpression);
-  }
-}
-
-function indexOfBlock(blocks: Array<Block>, style: Style) {
-  for (let i = 0; i < blocks.length; i++) {
-    if (style.block === blocks[i] || style.block.isAncestorOf(blocks[i])) {
-      return i;
+      return expr.path;
     }
   }
-  throw new Error("[internal error] Block not found.");
+
+  mustacheToStringExpression(builders: Syntax["builders"], stringExpression: Exclude<StringAST, null>): AST.Expression {
+    if (isConcatStatement(stringExpression)) {
+      return builders.sexpr(
+        builders.path(CONCAT_HELPER_NAME),
+        stringExpression.parts.reduce(
+          (arr, val) => {
+            if (val.type === "TextNode") {
+              arr.push(builders.string(val.chars));
+            } else {
+              arr.push(val.path);
+            }
+            return arr;
+          },
+          new Array<AST.Expression>()));
+    } else if (isSubExpression(stringExpression)) {
+      return stringExpression;
+    } else if (isPathExpression(stringExpression)) {
+      return builders.sexpr(stringExpression);
+    } else if (isMustacheStatement(stringExpression)) {
+      return this.mustacheToExpression(builders, stringExpression);
+    } else {
+      return assertNever(stringExpression);
+    }
+  }
 }
