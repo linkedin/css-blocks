f21e652d4707f55b669ff222cf6028b17466b35a
Chris Eppstein <chris@eppsteins.net>
2020-08-06 15:36:37 -0700

    fix: Don't use the css-blocks installation location for guid gen.

--- packages/@css-blocks/core/src/BlockParser/utils/genGuid.ts
+++ packages/@css-blocks/core/src/BlockParser/utils/genGuid.ts
@@ -5,9 +5,9 @@ import * as process from "process";
 const DEBUG = debugGenerator("css-blocks:caching");
 
 /**
- * Generates a unique identifier from a given input identifier. This
- * generated identifier hash will remain consistent for the tuple of (machine,
- * user, css blocks installation location).
+ * Generates a unique identifier from a given input identifier.
+ * This generated identifier hash will remain consistent for the tuple
+ * (identifier, machine, user).
  *
  * @param  identifier Input Block identifier.
  * @param  significantChars Number of characters from the start of the hash to use.
@@ -17,7 +17,6 @@ const DEBUG = debugGenerator("css-blocks:caching");
 export function gen_guid(identifier: string, signifcantChars: number): string {
   let hash = crypto.createHash("md5")
     .update(process.getuid().toString())
-    .update(__filename)
     .update(identifier)
     .digest("hex")
     .slice(0, signifcantChars);
