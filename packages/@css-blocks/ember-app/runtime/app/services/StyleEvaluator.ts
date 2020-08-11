fc7a346d03caeccf9d89b9cc7a227073369abcb9
Chris Eppstein <chris@eppsteins.net>
2020-08-10 17:41:55 -0700

    chore: Document the runtime style evaluator and resolver.

--- packages/@css-blocks/ember-app/runtime/app/services/StyleEvaluator.ts
+++ packages/@css-blocks/ember-app/runtime/app/services/StyleEvaluator.ts
@@ -29,6 +29,141 @@ const enum FalsySwitchBehavior {
   default = 2,
 }
 
+/**
+ * Evaluates the runtime state of the style expression provided by the CSS
+ * Blocks rewriter.
+ *
+ * The result of evaluating a style expression is a set of global style ids
+ * that are currently enabled on this element by the author.
+ *
+ * Note: The full set of style ids that are enabled on the element is not known
+ * until after style resolution.
+ *
+ * The style expression is a list of values that consists of four sections:
+ * 1. Metadata
+ * 2. Block References
+ * 3. Style References
+ * 4. Conditionals
+ *
+ * ## Metadata
+ *
+ * The metadata consists of a single value, an integer that represents a schema
+ * version that describes the format of the style expression. If the rewrite
+ * changes, we'll use this to simultaneously support the old and new version(s).
+ *
+ * ## Block References
+ *
+ * The purpose of the block references is to enumerate the list of blocks that
+ * the styles come from. These blocks are later referred to in the style
+ * expression according to a number that represents a zero-based index into the
+ * order they appear in the block reference portion of the style expression.
+ *
+ * Format: `[blockCount: number, (sourceBlockGuid: string, runtimeBlockGuid: string | null)+]`
+ *
+ * - `blockCount` is the number of `sourceBlockGuid`/`runtimeBlockGuid` pairs that follow.
+ * - `sourceBlockGuid` is the guid of the block that was referenced in the source code.
+ * - `runtimeBlockGuid` is the guid of a block that implements the
+ *   `sourceBlockGuid` interface and is meant to replace the source block at
+ *   runtime. The `runtimeBlockGuid` is null when no runtime block is passed.
+ *   This means the source block is used.
+ *
+ * ## Style References
+ *
+ * The purpose of Style References is to enumerate the list of styles that
+ * are referenced from the style conditionals that follow. Later references
+ * to these styles use the zero-based index into the order they appear in this
+ * section.
+ *
+ * Format: `[styleCount: number, (blockIndex: number, styleName: string)+]
+ * styleCount: The number of styles
+ *
+ * - `styleCount` is the number of styles that follow. Each style is a pair
+ *   of values.
+ * - `blockIndex` is the index that references the block of the style.
+ * - `styleName` is a string that uniquely identifies the style in the block
+ *   given as well as in any block that implements the same interface.
+ *
+ * ## Style Conditionals
+ *
+ * Style conditionals are how the possible authored styles are selected based
+ * on the runtime state of the application. There are 4 types of conditionals.
+ *
+ * The general form of the conditionals section of the style expression is
+ *
+ * Format: `[conditionalCount <conditional>+]`
+ *
+ * Where each `<conditional>` has a first argument that indicates the type
+ * of the conditional and the remaining arguments for each conditional are
+ * specific to that conditional type.
+ *
+ * The behavior of the conditional is to enable one or more of the possible
+ * styles on the element. All styles start out as disabled and require a conditional
+ * in order to cause that style to become enabled. A conditional never disables
+ * a style. Although rare, several conditionals might enable the same style.
+ *
+ * ### Static Conditional
+ *
+ * The static conditional enables a single style. No javascript-based state is
+ * taken into account.
+ *
+ * Type Value: `Condition.static` or `1`
+ * Format: [type: number, styleIndex: number]
+ *
+ * - `styleIndex`: An index of one of the styles provided in the style
+ *   reference portion of the style expression.
+ *
+ * ### Toggle Conditional
+ *
+ * A toggle conditional is used to enable one or more styles based on the
+ * "truthiness" of a javascript value.
+ *
+ * Format: `[type: number, value: boolean, styleCount: number, (styleIndex: number)+]`
+ *
+ * - `type`: `Condition.toggle` or `2`
+ * - `value`: A javascript value will be coerced to a boolean.
+ * - `styleCount`: The number of styles that are enabled when this condition is true.
+ * - `styleIndex`: An index of one of the styles provided in the style
+ *   reference portion of the style expression.
+ *
+ * ### Ternary Conditional
+ *
+ * A ternary conditional is used to enable 0 or more styles when a variable is
+ * true or to enable a different set of 0 or more styles when the same variable
+ * is false.
+ *
+ * Format: `[type: number, value: boolean,
+ *           trueCount: number, (trueStyleIndex: number)*,
+ *           falseCount: number, (falseStyleIndex: number)*]`
+ *
+ * - `type`: `Condition.ternary` or `3`
+ * - `value`: A javascript value will be coerced to a boolean.
+ * - `trueCount`: The number of styles that are enabled when this condition is true.
+ * - `trueStyleIndex`: An index of one of the styles provided in the style
+ *   reference portion of the style expression.
+ * - `falseCount`: The number of styles that are enabled when this condition is false.
+ * - `falseStyleIndex`: An index of one of the styles provided in the style
+ *   reference portion of the style expression.
+ *
+ * ### Switch Conditional
+ *
+ * A switch conditional is used to enable one or more styles based on the value
+ * of a string matching one of several possible values. This conditional is
+ * different from the others in that it doesn't select styles from style
+ * reference section and it is the only conditional that can result in a
+ * runtime error.
+ *
+ * Format: `[type: number, blockIndex: number,
+ *           attributeName: string, value: string | null]`
+ *
+ * - `type`: `Condition.switch` or `4`
+ * - `blockIndex`: An index that references a block from the block reference
+ *   portion of the style expression.
+ * - `attributeName`: A string that uniquely identifies the attribute in the
+ *   block given as well as in any block that implements the same interface.
+ * - `value`: A string that must match one of the attribute's values. If null
+ *   is returned then no style is enabled (undefined is an error). If the value
+ *   does not match one of the known attribute values then an error is raised.
+ */
 export class StyleEvaluator {
   data: AggregateRewriteData;
   args: ClassNameExpression;
--- packages/@css-blocks/ember-app/runtime/app/services/StyleResolver.ts
+++ packages/@css-blocks/ember-app/runtime/app/services/StyleResolver.ts
@@ -29,10 +29,88 @@ function assertNever(_value: never): never {
 
 type Condition = StyleCondition | ImplicationCondition;
 
+/**
+ * The goal of style resolution is to use the relationships declared between
+ * different CSS Block styles to infer the presence of some styles based
+ * on the current runtime set of styles applied explicitly by the author.
+ *
+ * In addition to adding styles to an element, style resolution can result in
+ * the removal of an explicitly applied style when the element's styles do not
+ * meet the requirements of those styles based on the presence of related
+ * styles.
+ *
+ * Specifically, style resolution handles the following CSS Block features:
+ * - Block Inheritance.
+ * - Stylesheet-based style composition.
+ * - Block aliases.
+ * - Class attributes only matching an element when the element has the class.
+ *
+ * This class implements a graph where the nodes are styles and directed edges
+ * are "implications". That is, the edge of (s1, s2) means that style s1 implies
+ * the presence of style s2.
+ *
+ * The graph's start node is a javascript Symbol named `root`. A style is
+ * considered to be applied to the element if it is reachable from the start
+ * node.
+ *
+ * We first populate the graph the graph with all possible styles that might be
+ * applied to the element and as we do so we record any requirements for the presence
+ * of that applied style because we can't know whether the requirements are properly
+ * met until all styles are added to the graph.
+ *
+ * Some requirements only remove one or more implications, which might leave
+ * the style node connected through some other path. Other requirements can
+ * cause the style node and all outbound implications to be removed.
+ *
+ * Because we use a unique number to represent a style, this graph is
+ * constructed a little differently from most graphs: There is no node type.
+ * Instead, we use a Map where the key is a style id and the value is a set of
+ * style ids implied by the key.
+ *
+ * The RuntimeDataGenerator emits data about the CSS Blocks styles that has
+ * undergone a significant amount of data preparation in order for this resolver
+ * to run without needing to perform its work in the domain of the CSS Block
+ * interfaces. Instead the CSS Block relationships are boiled down to implied
+ * styles and style requirements.
+ *
+ * An implied style takes one of three forms:
+ * - A style id (always implied)
+ * - A css class name (from an alias)
+ * - A conditional style implies one or more styles but only when the condition
+ * is satisfied. The condition is a boolean style expression of the same format
+ * that is used by the optimizer's output classnames. These conditions become a
+ * type of style requirement.
+ *
+ * A style requirement is a mapping from a style id to a boolean style
+ * expression that must evaluate to true in order for the style to be allowed
+ * to remain on the element.
+ *
+ * In order to enforce style requirements, once we've added all possible styles
+ * to the graph, we iterate over the requirements and remove any styles or
+ * implications that are not satisfied. If any styles are removed, this may
+ * cause a style requirement that previously was satisfied to become
+ * dissatisfied. Because of this we keep iterating over the list of style
+ * requirements until no styles are removed.
+ *
+ * During style requirements processing it's possible for sections of the graph
+ * to become disconnected from the root node, thus causing all the styles that
+ * were transitively implied by the removed style to also be removed from the
+ * element.
+ *
+ * Unfortunately, this means that during requirements processing we must
+ * perform a depth first search for any style that is mentioned in a boolean
+ * style expression. Fortunately, the size of the graph is generally small
+ * and relatively flat. However, there are ways to optimize the graph
+ * so that these reads are faster and in aggregate might result in a small
+ * runtime performance boost.
+ */
 export class StyleResolver {
+  /** source data for implied styles and style requirements. */
   data: AggregateRewriteData;
+  /** This is the implication graph. */
   implications: Map<number | typeof root, Set<number>>;
   _impliedClassNames: Map<number, Set<string>>;
+  /** Style requirements that must be satisfied. */
   conditionals: Array<Condition>;
   constructor(data: AggregateRewriteData) {
     this.data = data;
@@ -42,7 +120,7 @@ export class StyleResolver {
   }
 
   /**
-   * Returns true if the style is connected to the root node.
+   * Returns true if the style is currently connected to the root node.
    */
   public hasStyle(styleToFind: number): boolean {
     return this.hasStyleFrom(root, styleToFind);
@@ -59,16 +137,26 @@ export class StyleResolver {
     return false;
   }
 
+  /**
+   * Adds an explicitly declared style to the style resolver.
+   */
   public addStyle(style: number) {
     this.addImpliedStyle(root, style);
   }
 
+  /**
+   * This method mutates the graph and should only be called a single time
+   * once all styles are added.
+   */
   public resolve(): Set<number> {
     this.importRequirements();
     this.processConditions();
     return this.currentStyles();
   }
 
+  /**
+   * Gets all the implied class names for styles currently connected to root.
+   */
   public impliedClassNames(): Set<string> {
     let classNames = new Set<string>();
     for (let style of this.currentStyles()) {
@@ -93,6 +181,10 @@ export class StyleResolver {
     this._importImpliedStyles(style);
   }
 
+  /**
+   * Imports implied styles from the read only aggregate rewrite data into this
+   * resolver.
+   */
   private _importImpliedStyles(fromStyle: number) {
     let implied = this.data.impliedStyles[fromStyle];
     if (implied) {
@@ -108,6 +200,9 @@ export class StyleResolver {
     }
   }
 
+  /**
+   * Adds style implications and records the associated style requirements.
+   */
   private addCondition(fromStyle: number, condition: ConditionalStyle) {
     this.conditionals.push({
       type: IMPLICATION_TYPE,
@@ -149,6 +244,9 @@ export class StyleResolver {
     }
   }
 
+  /**
+   * Evaluates a boolean style expression.
+   */
   private evaluateExpression(expr: StyleExpression): boolean {
     if (typeof expr === "number") return this.hasStyle(expr);
     let op: Operator = expr[0];
@@ -193,6 +291,10 @@ export class StyleResolver {
     return wasRemoved;
   }
 
+  /**
+   * Helper function that is used to construct a set of all styles reachable
+   * from the root.
+   */
   private currentStylesFrom(fromStyle: number | typeof root, styles: Set<number>): void {
     if (fromStyle !== root) {
       styles.add(fromStyle);
